f7826ca51c821c17d684f4abf3aa6431a457263c
deepload <n.chappuis@outlook.com>
2025-03-04 22:41:19 +0100

    Fix

---
+++ temp/create-databases.sql
@@ -0,0 +1,24 @@
+﻿IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'OuiAI_Identity')
+BEGIN
+    CREATE DATABASE OuiAI_Identity;
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'OuiAI_Projects')
+BEGIN
+    CREATE DATABASE OuiAI_Projects;
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'OuiAI_Social')
+BEGIN
+    CREATE DATABASE OuiAI_Social;
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'OuiAI_Notifications')
+BEGIN
+    CREATE DATABASE OuiAI_Notifications;
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'OuiAI_Search')
+BEGIN
+    CREATE DATABASE OuiAI_Search;
+END
